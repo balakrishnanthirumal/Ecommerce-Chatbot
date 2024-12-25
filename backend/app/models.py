1e9f0f62cc6d358efa68720567f206cf630764e3
Naveen Kumar <168574579+workwithnaveen7@users.noreply.github.com>
2024-12-26 01:31:07 +0530

    Add files via upload

---
+++ backend/app/models.py
@@ -0,0 +1,118 @@
+from datetime import datetime
+from . import db
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
+    is_admin = db.Column(db.Boolean, default=False)
+    reviews = db.relationship('Review', backref='user', lazy=True)
+    orders = db.relationship('Order', backref='user', lazy=True)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at,
+            'is_admin': self.is_admin
+        }
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    in_stock = db.Column(db.Boolean, default=True)
+    description = db.Column(db.Text, nullable=False)
+    image = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    review_count = db.Column(db.Integer, default=0)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
+    reviews = db.relationship('Review', backref='product', lazy=True)
+    order_items = db.relationship('OrderItem', backref='product', lazy=True)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'category_id': self.category_id,
+            'in_stock': self.in_stock,
+            'description': self.description,
+            'image': self.image,
+            'price': self.price,
+            'rating': self.rating,
+            'review_count': self.review_count,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
+        }
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    order = db.Column(db.String(200), nullable=False)
+    payment_id = db.Column(db.String(100), nullable=False)
+    status = db.Column(db.String(50), nullable=False)
+    address = db.Column(db.String(200), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    order_items = db.relationship('OrderItem', backref='order', lazy=True)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'order': self.order,
+            'payment_id': self.payment_id,
+            'status': self.status,
+            'address': self.address,
+            'created_at': self.created_at,
+            'user_id': self.user_id
+        }
+
+class OrderItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'product_id': self.product_id,
+            'order_id': self.order_id,
+            'quantity': self.quantity
+        }
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
+    products = db.relationship('Product', backref='category', lazy=True)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
+        }
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    review = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'product_id': self.product_id,
+            'review': self.review,
+            'created_at': self.created_at
+        }
