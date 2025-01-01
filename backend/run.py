6a3c6448c3974d6de9f4e9d3046cea6b30d65913
balakrishnanthirumal <balakrishnanthirumalt@gmail.com>
2025-01-01 19:04:24 +0530

    done

--- backend/run.py
+++ backend/run.py
@@ -1,7 +1,9 @@
 from app import create_app
+from flask import Flask
 
 app = create_app()
-wsgi = app
+
+app = Flask(__name__)
 #this is the entry point for the application. It creates an instance of the Flask application and runs it in debug mode.
 if __name__ == '__main__':
     app.run(debug=True)
