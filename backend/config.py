bc3841e64e737e4ec3f6bf8b241fad82318a8b32
Naveen Kumar <168574579+workwithnaveen7@users.noreply.github.com>
2025-01-01 18:06:41 +0530

    Delete backend directory

--- backend/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    #set secret key to use session in Flask
-    SECRET_KEY = os.getenv('SECRET_KEY', 'supersecretkey')
-    #set the database
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///ecommerce.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- backend/keygen.py
+++
@@ -1,4 +0,0 @@
-import secrets
-#to generate a random secret key
-secret_key = secrets.token_hex(32)
-print(secret_key)
